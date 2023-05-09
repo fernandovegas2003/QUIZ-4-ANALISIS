9ec9c562010127e24473ff5395e4bd097a5967aa
QksCookies <128483888+QksCookies@users.noreply.github.com>
2023-05-09 03:05:28 -0500

    Update ControllerTest.java

--- AdministradorListaTareas/src/test/java/ControllerTest.java
+++ AdministradorListaTareas/src/test/java/ControllerTest.java
@@ -4,7 +4,12 @@
  */
 
 import com.todo.model.administradorlistatareas.Task;
+import com.todo.controller.administradorlistatareas.TaskController;
+import com.todo.view.administradorlistatareas.ConsoleView;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
@@ -12,55 +17,101 @@ import static org.junit.jupiter.api.Assertions.*;
  * @author julic
  */
 public class ControllerTest {
+private TaskController controller;
+    private ConsoleView view;
     
-private ControllerTest controllerTest;
-    private Task task1;
-    private Task task2;
-    private Task task3;
-    
+    @BeforeEach
     public void setUp() {
-        controllerTest = new ControllerTest();
-        task1 = new Task("Comprar leche", "1001", "Ir al supermercado y comprar leche", "pendiente");
-        task2 = new Task("Ir al médico", "1002", "Ir al hospital para chequeo médico", "en progreso");
-        task3 = new Task("Pagar facturas", "1003", "Pagar facturas de servicios públicos", "completado");
+        view = new ConsoleView();
+        controller = new TaskController(view);
     }
     
     @Test
-    public void testAgregarTask() {
-        controllerTest.agregarTask(task1);
-        assertEquals(1, controllerTest.getTasks().size());
-        assertEquals(task1, controllerTest.getTasks().get(0));
+    public void testAgregarTarea() {
+        // Arrange
+        String titulo = "Hacer la tarea";
+        String descripcion = "Hacer la tarea de matemáticas antes del viernes";
+        String estado = "false";
+
+        // Act
+        controller.agregarTarea(titulo, descripcion, estado);
+
+        // Assert
+        List<Task> tareas = (List<Task>) controller.getTareas();
+        assertNotNull(tareas);
+        assertEquals(1, tareas.size());
+        Task tarea = tareas.get(0);
+        assertEquals(titulo, tarea.getTitulo());
+        assertEquals(descripcion, tarea.getDescripcion());
+        assertFalse(tarea.getEstado());
     }
     
     @Test
-    public void testEliminarTask() {
-        controllerTest.agregarTask(task1);
-        controllerTest.agregarTask(task2);
-        controllerTest.agregarTask(task3);
-        assertTrue(controllerTest.eliminarTask("Ir al médico", "1002"));
-        assertEquals(2, controllerTest.getTasks().size());
-        assertFalse(controllerTest.getTasks().contains(task2));
+    public void testVerTareasSinTareas() {
+        // Arrange
+        String mensajeEsperado = "No hay tareas.";
+        
+        // Act
+        controller.verTareas();
+        
+        // Assert
+        assertEquals(mensajeEsperado, view.getMensaje());
     }
     
     @Test
-    public void testEliminarTaskNoEncontrado() {
-        controllerTest.agregarTask(task1);
-        assertFalse(controllerTest.eliminarTask("Ir al médico", "1002"));
-        assertEquals(1, controllerTest.getTasks().size());
+    public void testVerTareasConTareas() {
+        // Arrange
+        String titulo = "Hacer la tarea";
+        String descripcion = "Hacer la tarea de matemáticas antes del viernes";
+        boolean estado = false;
+        controller.agregarTarea(titulo, descripcion, estado);
+        String mensajeEsperado = titulo + ": " + descripcion + " (Pendiente)";
+        
+        // Act
+        controller.verTareas();
+        
+        // Assert
+        assertEquals(mensajeEsperado, view.getMensaje());
     }
     
     @Test
-    public void testBuscarTask() {
-        controllerTest.agregarTask(task1);
-        controllerTest.agregarTask(task2);
-        controllerTest.agregarTask(task3);
-        assertEquals(task2, controllerTest.buscarTask("Ir al médico", "1002"));
+    public void testActualizarTarea() {
+        // Arrange
+        ConsoleView vista = new ConsoleView();
+        TaskController controller = new TaskController(vista);
+        Task tarea = new Task("Hacer la tarea", "Hacer la tarea de matemáticas antes del viernes", false);
+        controller.agregarTarea(tarea);
+
+        vista.agregarOpcion("0");
+        vista.agregarOpcion("2");
+        vista.agregarEntrada("Hacer la tarea actualizada");
+        vista.agregarEntrada("Hacer la tarea de matemáticas antes del sábado");
+
+        // Act
+        controller.actualizarTarea();
+
+        // Assert
+        List<Task> tareas = (List<Task>) controller.getTareas();
+        assertEquals(1, tareas.size());
+
+        tarea = tareas.get(0);
+        assertEquals("Hacer la tarea actualizada", tarea.getTitulo());
+        assertEquals("Hacer la tarea de matemáticas antes del sábado", tarea.getDescripcion());
     }
     
     @Test
-    public void testBuscarTaskNoEncontrado() {
-        controllerTest.agregarTask(task1);
-        assertNull(controllerTest.buscarTask("Ir al médico", "1002"));
+    public void testEliminarTarea() {
+        // Arrange
+        String titulo = "Hacer la tarea";
+        String descripcion = "Hacer la tarea de matemáticas antes del viernes";
+        boolean estado = false;
+        controller.agregarTarea(titulo, descripcion, estado);
+        
+        // Act
+        controller.eliminarTarea(1);
+        
+        // Assert
+        assertTrue((boolean) controller.getTareas());
     }
     
 }
